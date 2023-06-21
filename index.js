2e0d0dba44e18dbcb407ef1aa9de3a95c5072a48
Alyssa <alyssa.brooksong@gmail.com>
2023-06-21 13:55:30 -0700

    second deliverable done

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const URL = 'http://localhost:3000/menu'
 
 // DOM Selectors
-const menu = document.querySelector('#menu-items')
+const menu = document.querySelector('#menu-items');
+const dishImage = document.querySelector('#dish-name');
+const dishName = document.querySelector('#dish-name');
+const dishDescription = document.querySelector('#dish-price');
+const numberInCart = document.querySelector('#number-in-cart')
+
 // Fetch functions
 function getAllDishes(url) {
     return fetch(url)
@@ -18,6 +23,14 @@ function renderInMenu(dishObj){
     const span = document.createElement("span");
     span.textContent = dishObj.name;
     menu.append(span)
+}
+
+function renderDetail(dishObj){
+    dishImage.src = dishObj.dish
+    dishName.textContent = dishObj.name
+    dishDescription.textContent = dishObj.description
+    dishPrice.textContent = dishObj.price
+    numberInCart.textContent = dishObj.number_in_bag
 }
 
 // Intializers
--- notes.md
+++ notes.md
@@ -8,6 +8,10 @@ As a user I can:
 - [X] Append each <span> to anchor point
 
 2. View the details of the first Menu item in Dish detail secion on page load
+- [X] Select DOM elements to modify
+- [X] Pass one dish to render function
+- [X] Set element attributes with data from dishObj
+
 3. Click a menu item and see it's detaisl displayed in the Dish detail section
 4. Add menu items to 'my cart' Cart only needs to increment when the same dish is selected, but can reset when another is selected
 
